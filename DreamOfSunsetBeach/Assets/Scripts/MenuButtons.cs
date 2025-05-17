9c9abaaeda2d722baf10b91b1fd47422589219e1
Natsume <natsumeniichan@gmail.com>
2025-05-17 17:11:54 +0200

    Menu initiated

--- DreamOfSunsetBeach/Assets/Scripts/MenuButtons.cs
+++ DreamOfSunsetBeach/Assets/Scripts/MenuButtons.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using System.Linq;
+using System;
 
 public class MenuButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject menuScreen;
+    private GameObject optionScreen;
+    private void Awake()
     {
-        
+        menuScreen = GameObject.Find("MainScreen");
+        optionScreen = GameObject.Find("OptionScreen");
+
+        optionScreen.SetActive(false);
+    }
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadButton()
     {
-        
+
+    }
+
+    public void OptionsButton()
+    {
+        menuScreen.SetActive(false);
+        optionScreen.SetActive(true);
+    }
+
+    public void CreditButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void EnButton()
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
+
+    }
+
+    public void DeButton()
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+    }
+
+    public void SoundButton()
+    {
+
+    }
+
+    public void BackButton()
+    {
+        menuScreen.SetActive(true);
+        optionScreen.SetActive(false);
     }
 }
